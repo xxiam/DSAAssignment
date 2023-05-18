b8fd3ab15b3304c7653ff2d5c536b696589901a4
xxiam <jhaanrei@gmail.com>
2023-05-19 02:35:29 +0800

    added hashTable

--- DSAAssignment.py
+++ DSAAssignment.py
@@ -1,16 +1,16 @@
 #using graph, hash table, heap
 import Dependancies.DSAGraph as DSAGraph
-
+import Dependancies.DSAhash as DSAhash
 def main():
 
     #A 32 45 90
     #location : temperature, humidity, wind speed
 
-
     graph = DSAGraph.Graph()
+    hashTable = DSAhash.DSAHashTable(graph.getVertCount())
     graph.importFile("location.txt", "UAVdata.txt") #finished importing
-
-    graph.sortVertices()
+    print("Graph adjaceny list")
+    graph.displayAsList()
 
 if __name__ == "__main__":
     main()
--- scalabilityProgram.py
+++ scalabilityProgram.py
@@ -1,2 +1,29 @@
 import Dependancies.DSAGraph as DSAGraph
 
+def main():
+    '''
+    scalability program to create, edit, and remove vertices and edges
+    '''
+    
+    print("Scalabiltiy Program")
+    userInput = input("> ")
+
+    '''
+    possible choices
+    - import file : 
+    - export file :
+    - add vertex : 
+    - add edge : 
+    - remove vertex : 
+    - remove edge : 
+    - display as list : 
+    - edit weight of edge : 
+    '''
+
+    while userInput != "q":
+        ...
+        
+
+
+if __name__ == "__main__":
+    main()
