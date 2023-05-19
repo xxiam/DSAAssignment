417d315455b5cf340f4ccbc3a2319bb816c3be1a
xxiam <jhaanrei@gmail.com>
2023-05-19 17:16:39 +0800

    started scalability program

--- scalabilityProgram.py
+++ scalabilityProgram.py
@@ -20,10 +20,62 @@ def main():
     - edit weight of edge : 
     '''
 
-    while userInput != "q":
-        ...
-        
+    #init objects
+    graph = DSAGraph.Graph()
 
+    while userInput[0] != "q":
+        userInput = input("> ").split()
+
+        if userInput[0] == "help":
+            print("help: ")
+            print("import <location file> <data file>")
+            print("export <location filename> <data filename>")
+            print("add <vertex label> <vertex value>")
+            print("addEdge <vertex1> <vertex2> <weight>")
+            print("remove <vertex label>")
+            print("removeEdge <vertex1> <vertex2>")
+            print("edit <vertex>")
+            print("display")
+
+        elif userInput[0] == "import":
+            locationPath = userInput[1]
+            dataPath = userInput[2]
+            graph.importFile(locationPath, dataPath)
+
+        elif userInput[0] == "export":
+            #create export method in DSAgraph.py
+            ...
+
+        elif userInput[0] == "add":
+            label = userInput[1]
+            value = userInput[2]
+            graph.addVertex(label, value)
+
+        elif userInput[0] == "addEdge":
+            vertex1 = userInput[1]
+            vertex2 = userInput[2]
+            weight = userInput[3]
+            graph.addTwoWay(vertex1, vertex2, weight)
+        
+        elif userInput[0] == "remove":
+            label = userInput[1]
+            graph.removeVertex(label)
+
+        elif userInput[0] == "removeEdge":
+            vertex1 = userInput[1]
+            vertex2 = userInput[2]
+            graph.removeEdge(vertex1, vertex2)
+
+        elif userInput[0] == "edit":
+            editVertex(userInput[1])
+
+        elif userInput[0] == "display":
+            graph.displayAsList()
+
+    print("exiting")
+
+def editVertex(vertexObject:DSAGraph.GraphVertex):
+    ...
 
 if __name__ == "__main__":
     main()
